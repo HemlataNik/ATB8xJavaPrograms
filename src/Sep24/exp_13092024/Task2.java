8429d0b561d55c95eb73a7e2349aeaaf091d2698
HemlataNik <morehemlata36@gmail.com>
2024-09-15 15:44:02 -0400

    Adding new source file

---
+++ src/Sep24/exp_13092024/Task2.java
@@ -0,0 +1,25 @@
+package Sep24.exp_13092024;
+
+public class Task2 {
+    public static void main (String[] args){
+        //write code for simple calculator
+        //add of two nos: a+b
+        //sub of two nos: a-b
+        //div of two nos: a/b
+        //mul of two nos: a*b
+        //mod of two nos: a%b
+        float a = 50;
+        float b = 22;
+        System.out.printf("%f + %f= %f",a, b,(a+b));
+        System.out.println();
+        System.out.printf("%f - %f= %f",a, b,(a-b));
+        System.out.println();
+        System.out.printf("%f * %f= %f",a, b,(a*b));
+        System.out.println();
+        System.out.printf("%f / %f= %f",a, b,(a/b));
+        System.out.println();
+
+
+
+    }
+}
