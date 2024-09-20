dd193e8b9263cf79a75d27a8ec85146c0f8e5a73
HemlataNik <morehemlata36@gmail.com>
2024-09-20 18:40:05 -0400

    Adding Task files

--- src/Sep24/Tasks/Task_20092024.java
+++ src/Sep24/Tasks/Task_20092024.java
@@ -1,4 +1,32 @@
 package Sep24.Tasks;
 
 public class Task_20092024 {
+    public static void main(String[] args) {
+        //condition ? expression_if_true: expression_if_false
+
+
+        //By using Ternary Operators max between 3 numbers.
+        // Input int - a,b,c - a = 10, b = 20, c = 45
+        // Max → a,b,c → c
+
+        int a = 10;
+        int b = 20;
+        int c = 45;
+
+        String max = (a > b && a > c) ? "a" : (b > a && b > c) ? "b" : (c > a && c > b) ? "c" : "Two numbers are equal";
+        System.out.println(max);
+
+//Ternary Operators to handle multiple conditions.
+
+// Input → int score = 85
+
+// String grade →
+//  score >= 90 → A
+//  score >= 80 → B
+//  score >= 70 → C
+        int score = 85;
+
+        String grade = (score >= 90) ? "A" : (score >= 80) ? "B" : (score >= 70) ? "C" : "F";
+        System.out.println(grade);
+    }
 }
