64a43f36a4d3b997adc7b5f1df06d49d95e483da
HemlataNik <morehemlata36@gmail.com>
2024-09-20 18:52:23 -0400

    Triagles_Classifier code added

---
+++ src/Sep24/Tasks/Task_TriangleClassifier_20092024.java
@@ -0,0 +1,30 @@
+package Sep24.Tasks;
+
+public class Task_TriangleClassifier_20092024 {
+    public static void main(String[] args) {
+
+// Write a program that classifies a triangle based on its side lengths.
+
+// Given three input values representing the lengths of the sides, determine
+
+// if the triangle is equilateral (all sides are equal),
+
+// isosceles (exactly two sides are equal), or
+
+// scalene (no sides are equal).
+
+// Use an if-else statement to classify the triangle.
+
+// side1, side2, side3 ->
+        int side1 = 67; // Replace with the lengths of your triangle's sides
+        int side2 = 45;
+        int side3 = 67;
+        if (side1 == side2 && side2 == side3) {
+            System.out.println("Equilateral triangle");
+        } else if (side1 == side2 || side1 == side3 || side2 == side3) {
+            System.out.println("Isosceles triangle");
+        } else {
+            System.out.println("Scalene triangle");
+        }
+    }
+}
